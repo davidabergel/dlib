2091d60e1574567398ea5bc1c38ff68cc8b8d8eb
David Abergel <david.abergel@nordita.org>
2017-04-28 09:16:21 +0200

    Added some gsl complex add/prod functions

--- d_functions.cpp
+++ d_functions.cpp
@@ -4,3 +4,35 @@ double dlib::stepsize( double xmin, double xmax, int xpts )
 {
 	return (xmax-xmin)/(double)(xpts-1);
 }
+
+gsl_complex dlib::gslc_sum( gsl_complex a, gsl_complex b )
+{
+	return gsl_complex_add( a, b );
+}
+
+gsl_complex dlib::gslc_sum( gsl_complex a, gsl_complex b, gsl_complex c )
+{
+	return gsl_complex_add( a, gsl_complex_add( b, c ) );
+}
+
+gsl_complex dlib::gslc_sum( gsl_complex a, gsl_complex b, gsl_complex c, 
+			gsl_complex d )
+{
+	return gsl_complex_add( gsl_complex_add(a,b), gsl_complex_add(c,d) );
+}
+
+gsl_complex dlib::gslc_prod( gsl_complex a, gsl_complex b )
+{
+	return gsl_complex_mul( a, b );
+}
+
+gsl_complex dlib::gslc_prod( gsl_complex a, gsl_complex b, gsl_complex c )
+{
+	return gsl_complex_mul( a, gsl_complex_mul( b, c ) );
+}
+
+gsl_complex dlib::gslc_prod( gsl_complex a, gsl_complex b, gsl_complex c, 
+			gsl_complex d )
+{
+	return gsl_complex_mul( gsl_complex_mul(a,b), gsl_complex_mul(c,d) );
+}
--- d_functions.h
+++ d_functions.h
@@ -1,8 +1,21 @@
 #ifndef D_FUNCTIONS_h
 
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+
 namespace dlib
 {
 	double stepsize( double xmin, double xmax, int xpts );
+
+	gsl_complex gslc_sum( gsl_complex a, gsl_complex b );
+	gsl_complex gslc_sum( gsl_complex a, gsl_complex b, gsl_complex c );
+	gsl_complex gslc_sum( gsl_complex a, gsl_complex b, gsl_complex c, 
+			gsl_complex d );
+
+	gsl_complex gslc_prod( gsl_complex a, gsl_complex b );
+	gsl_complex gslc_prod( gsl_complex a, gsl_complex b, gsl_complex c );
+	gsl_complex gslc_prod( gsl_complex a, gsl_complex b, gsl_complex c, 
+			gsl_complex d );
 }
 
 #endif
